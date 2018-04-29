f12c94ba4a931819a8520afb7f0d056a03812603
Diego Silva <dsilva2401@gmail.com>
2018-04-29 18:26:01 -0500

    Seed bug fixed

--- src/seed/src/processes/server/index.js
+++ src/seed/src/processes/server/index.js
@@ -3,8 +3,11 @@ import yaml from 'js-yaml';
 import path from 'path';
 import fs from 'fs';
 
+var procSettingsPath = path.join(__dirname, './settings.yaml');
+var procSettings = yaml.safeLoad(fs.readFileSync(procSettingsPath, 'utf-8'));
+
 // Init process handler
-var processHandler = new ProcessHandler('./settings.yaml');
+var processHandler = new ProcessHandler(procSettings);
 
 processHandler.set('demo-handler', (context, next, finish) => {
     finish(200, 'Hello world');
