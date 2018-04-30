5d39de4a4d5680ad50c0c3c2ae07251071388da7
Diego Silva <dsilva2401@gmail.com>
2018-04-29 22:26:46 -0500

    CLI updated

--- src/cli/initProject.js
+++ src/cli/initProject.js
@@ -1,5 +1,9 @@
 var fsExtra = require('fs-extra');
+var shell = require('shelljs');
 
 module.exports = function (seedPath, targetPath) {
+    console.log('Setting up project');
     fsExtra.copySync(seedPath, targetPath);
+    console.log('Installing dependencies');
+    shell.exec('sh -c \'cd '+targetPath+' && npm install\'');
 }
